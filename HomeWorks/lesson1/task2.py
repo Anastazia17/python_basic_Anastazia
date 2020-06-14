8674fcd1fddb39ac176074b3b6ba5db939468d44
Anastazia17 <stasya17kolomna@gmail.com>
2020-06-14 14:26:40 +0300

    del directory lesson1 del file task1.py del file task2.py del file task3.py del file task4.py del file task5.py del file task6.py

--- HomeWorks/lesson1/task2.py
+++
@@ -1,7 +0,0 @@
-# Add task2.py
-
-var_time = int(input('Введите время в секундах: '))
-var_hours = var_time // 3600
-var_minutes = (var_time - var_hours * 3600) // 60
-var_seconds = var_time - (var_hours * 3600 + var_minutes * 60)
-print(f'Время в формате чч:мм:сс: {var_hours:02}:{var_minutes:02}:{var_seconds:02}')
--- HomeWorks/lesson1/task3.py
+++
@@ -1,5 +0,0 @@
-# Add task3.py
-
-n = int(input('Введите число: '))
-result = (n + int(str(n) + str(n)) + int(str(n) + str(n)+ str(n)))
-print('Сумма чисел n + nn + nnn: %d' % result)
