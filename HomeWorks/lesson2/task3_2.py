bae8734e63322751a38e7572148433ee2232598c
Anastazia17 <stasya17kolomna@gmail.com>
2020-06-14 18:22:54 +0300

    done task3_2.py

--- HomeWorks/lesson2/task3_2.py
+++ HomeWorks/lesson2/task3_2.py
@@ -1,2 +1,17 @@
 # Add task3_2.py
 
+while True:
+    seasons_dict = {'key_1' : 'зима', 'key_2' : 'весна', 'key_3' : 'лето', 'key_4' : 'осень'}
+    month = int(input('Введите порядковый номер месяца: '))
+
+    if month == 1 or month == 12 or month == 2:
+        print('Это', seasons_dict.get('key_1'))
+    elif month == 3 or month == 4 or month ==5:
+        print('Это', seasons_dict.get('key_2'))
+    elif month == 6 or month == 7 or month == 8:
+        print('Это', seasons_dict.get('key_3'))
+    elif month == 9 or month == 10 or month == 11:
+        print('Это', seasons_dict.get('key_4'))
+    else:
+        print('Введен некорректный номер месяца!')
+        continue
