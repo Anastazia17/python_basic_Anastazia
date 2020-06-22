6afd12fb5a0b7e48ed33736a9dfa41e99afa5e94
Anastazia17 <stasya17kolomna@gmail.com>
2020-06-22 21:04:08 +0300

    changed task2.py

--- HomeWorks/lesson4/task2.py
+++ HomeWorks/lesson4/task2.py
@@ -1,4 +1,4 @@
-# Add task.2
+# Add task.2.py
 
 my_list = [600, 2, 24, 12, 44, 1, 3, 899, 2, 1, 4, 10, 7, 1, 78, 123, 900, 69, 55, 117]
 my_new_list = [my_list[i] for i in range(1, len(my_list)) if my_list[i] > my_list[i - 1]]
