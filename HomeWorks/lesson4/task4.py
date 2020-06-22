9de202fa379d1e7faef30057e0e86ba9d8ca1bad
Anastazia17 <stasya17kolomna@gmail.com>
2020-06-22 21:22:47 +0300

    changed task2.py changed task3.py changed task4.py

--- HomeWorks/lesson4/task4.py
+++ HomeWorks/lesson4/task4.py
@@ -1,4 +1,4 @@
-# Add task.4.py
+# Add task4.py
 
 my_list = [1, 1, 5, 2, 2, 6, 2, 7, 23, 1, 44, 56, 44, 3, 2, 9, 10, 10, 7, 4, 11, 77, 99, 99]
 new_list = [el for el in my_list if my_list.count(el) < 2]
