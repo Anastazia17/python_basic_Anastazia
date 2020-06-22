9f1af4a376168b96760e7d0f5cce0d2bf24fd212
Anastazia17 <stasya17kolomna@gmail.com>
2020-06-22 21:33:52 +0300

    done task5.py

--- HomeWorks/lesson4/task5.py
+++ HomeWorks/lesson4/task5.py
@@ -1,2 +1,9 @@
 # Add task.5.py
 
+from functools import reduce
+
+def my_func(el_i, el):
+    return el_i * el
+
+print(f'Список всех четных чисел от 100 до 1000: {[el for el in range(100, 1001) if el % 2 == 0]}')
+print(f'Результат произведения всех элементов списка: {reduce(my_func, [el for el in range(100, 1001) if el % 2 == 0])}')
