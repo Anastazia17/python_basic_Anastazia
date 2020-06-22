a69d99d0aaa3ebf2b4c8692001a44250a6d2ae6c
Anastazia17 <stasya17kolomna@gmail.com>
2020-06-22 20:33:24 +0300

    done task1.py

--- HomeWorks/lesson4/task1.py
+++ HomeWorks/lesson4/task1.py
@@ -1,2 +1,13 @@
 # Add task1.py
 
+from sys import argv
+def calc_wage (hours, wage, premium):
+    calc_wage = int(hours) * int(wage) + int(premium)
+    print(f'Заработная плата сотрудника с учетом премии: {calc_wage} RUB')
+
+try:
+    script_name, hours, wage, premium = argv
+    calc_wage(hours, wage, premium)
+
+except ValueError:
+    print('Введено не три параметра или введены не числа!')
