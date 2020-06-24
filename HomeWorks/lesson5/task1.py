5704d4c5467a9612a17216d0820421e3d2f44461
Anastazia17 <stasya17kolomna@gmail.com>
2020-06-24 16:47:36 +0300

    added test1.txt done task1.py

--- HomeWorks/lesson5/task1.py
+++ HomeWorks/lesson5/task1.py
@@ -1,2 +1,13 @@
 # Add task1.py
 
+with open('test1.txt', 'w') as my_f:
+    data = input('Введите текст: ')
+    while data:
+        my_f.writelines(data + '\n')
+        data = input('Введите текст: ')
+        if data == None:
+            break
+
+with open('test1.txt') as my_f:
+    for line in my_f:
+        print(line)
