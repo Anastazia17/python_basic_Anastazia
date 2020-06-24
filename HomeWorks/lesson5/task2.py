a49428b729fc1e4f6b06723cf0c5305d5ce0d741
Anastazia17 <stasya17kolomna@gmail.com>
2020-06-24 17:13:34 +0300

    done task2.py

--- HomeWorks/lesson5/task2.py
+++ HomeWorks/lesson5/task2.py
@@ -1,2 +1,20 @@
 # Add task2.py
 
+with open('test2.txt', encoding='UTF-8') as my_f:
+    content = my_f.read()
+    print(f'Содержимое файла:\n{content}')
+
+with open('test2.txt', encoding='UTF-8') as my_f:
+    content = my_f.readlines()
+    print(f'Количество строк в файле: {len(content)}')
+
+with open('test2.txt', encoding='UTF-8') as my_f:
+    content = my_f.readlines()
+    for i in range(len(content)):
+        print(f'Количество символов {i + 1}-ой строки: {len(content[i])}')
+# Количество символов в каждой строке, включая переносы строк (они тоже считаются за символы)
+
+with open('test2.txt', encoding='UTF-8') as my_f:
+    content = my_f.read()
+    content = content.split()
+    print(f'Общее количество слов в строках: {len(content)}')
