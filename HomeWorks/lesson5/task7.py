7adf48745b5ca232c5afc317e2a4c94d08e326a8
Anastazia17 <stasya17kolomna@gmail.com>
2020-06-26 09:23:03 +0300

    changed task7.py changed test7.txt changed test7.json

--- HomeWorks/lesson5/task7.py
+++ HomeWorks/lesson5/task7.py
@@ -20,13 +20,13 @@ with open('test7.txt', 'r', encoding='UTF-8') as my_f:
         print(f'Средняя прибыль всех фирм: {average_profit:.2f}')
     else:
         print(f'Средняя прибыль отсутсвует. Все фирмы в убытке.')
-    total = {'Total average profit': round(average_profit)}
+    total = {'Общая средняя прибыль': round(average_profit)}
     profits.update(total)
     print(f'Прибыль каждой компании: {profits}')
 
 with open('test7.json', 'w', encoding='UTF-8') as write_js:
     json.dump(profits, write_js)
 
-    js_str = json.dumps(profits)
+    js_str = json.dumps(profits, ensure_ascii=False, separators=(',', ': '), indent=4)
     print(f'Был создан файл json со следующим содержимым:\n'
           f' {js_str}')
