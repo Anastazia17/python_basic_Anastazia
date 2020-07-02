c9ed3a446c845b6a5a0bb18dd24c8ee67320b949
Anastazia17 <stasya17kolomna@gmail.com>
2020-07-02 15:01:07 +0300

    done task1.py

--- HomeWorks/lesson7/task1.py
+++ HomeWorks/lesson7/task1.py
@@ -1,2 +1,25 @@
 # Add task1.py
 
+class Matrix:
+    def __init__(self, list_1, list_2):
+        self.list_1 = list_1
+        self.list_2 = list_2
+    def __add__(self):
+        mat_list = [[0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]]
+        for i in range(len(self.list_1)):
+            for j in range(len(self.list_2[i])):
+                mat_list[i][j] = self.list_1[i][j] + self.list_2[i][j]
+        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in mat_list]))
+    def __str__(self):
+        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in mat_list]))
+
+new_matrix = Matrix([[9, 24, 15],
+                    [10, 21, 27],
+                    [45, 54, 13]],
+                   [[49, 12, 7],
+                    [11, 15, 99],
+                    [27, 9, 107]])
+
+print(new_matrix.__add__())
