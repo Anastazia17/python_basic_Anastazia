c15c335e2e898615e47ebd8218896a2b8b58caea
Anastazia17 <stasya17kolomna@gmail.com>
2020-07-08 15:25:07 +0300

    done task4-6.py

--- HomeWorks/lesson8/task4-6.py
+++ HomeWorks/lesson8/task4-6.py
@@ -1,56 +1,94 @@
 # Add task4-6.py
 
-class Stock:
-    def __init__(self, name, cost, quantity, list_number, *args):
-        self.name = name
-        self.cost = cost
-        self.quantity = quantity
-        self.list_number = list_number
-        self.full_stock = []
-        self.list_stock = []
-        self.dict_stock = {'Наименование устройства': self.name, 'Стоимость': self.cost, 'Количество на складе': self.quantity}
+from random import choice
 
-    def __str__(self):
-        return f'{self.name} стоимостью {self.cost} руб. в количестве {self.quantity} шт.'
+class Storage:
+    types = ['Printers', 'Scanners', 'Xeroxes']
+    stock = {}
+    departmens = ['Отдел продаж', 'Отдел развития', 'Отдел разработки',
+                  'Отдел качества', 'Отдел маркетинга', 'Отдел кадров', 'Отдел бухгалтерии']
 
-    def input_stock(self):
+    @classmethod
+    def tech_entry(cls, other):
         try:
-            elem1 = input(f'Введите наименование устройства: ')
-            elem2 = int(input('Введите стоимость за штуку: '))
-            elem3 = int(input('Введите количество на складе: '))
-            device = {'Наименование устройства': elem1, 'Стоимость': elem2, 'Количество на складе': elem3}
-            self.dict_stock.update(device)
-            self.list_stock.append(self.dict_stock)
-            print(f'Текущий список:\n{self.list_stock}')
-        except ValueError as error:
-            return f'Введены некорректные данные!'
-        print('Чтобы продолжить, нажмите Enter\nДля выхода нажмите Q')
-        choice = input('')
-        if choice == 'Q' or choice == 'q':
-            self.full_stock.append(self.list_stock)
-            print(f'Товары на складе:\n{self.full_stock}')
-            return f'Программа завершена'
-        else:
-            return Stock.input_stock(self)
+            if other.type not in cls.types:
+                raise UserWarning('Технику такого типа нельзя принять на склад!')
+            else:
+                if other.model in cls.stock:
+                    cls.stock[other.model][1]+=other.amount
+                else:
+                    cls.stock[other.model] = [other.type, other.amount]
+                print(f'Техника {other.model} принята на склад в количестве {cls.stock[other.model][1]} штук.')
+        except UserWarning as e:
+            print(e)
 
-class Printers(Stock):
-    def print(self):
-        return f'Отправить на печать {self.list_number} раз'
+    @classmethod
+    def tech_transfer(cls):
+        for model, item in cls.stock.items():
+            the_type = item[0]
+            if the_type == 'Printers':
+                if cls.stock[model][1] == 0:
+                    print(f'На складе нет техники {model} для передачи!')
+                else:
+                    cls.stock[model][1]-=1
+                    print(f'Техника {model} передана в {choice(cls.departmens)}.')
+            if the_type == 'Scanners':
+                if cls.stock[model][1] == 0:
+                    print(f'На складе нет техники {model} для передачи!')
+                else:
+                    cls.stock[model][1]-=1
+                    print(f'Техника {model} передана в {choice(cls.departmens)}.')
+            if the_type == 'Xeroxes':
+                if cls.stock[model][1] == 0:
+                    print(f'На складе нет техники {model} для передачи!')
+                else:
+                    cls.stock[model][1] -= 1
+                    print(f'Техника {model} передана в {choice(cls.departmens)}.')
+        print()
 
-class Scanners(Stock):
-    def scan(self):
-        return f'Отправить на сканирование {self.list_number} раз'
+class OfficeTech():
+    def __init__(self, model, amount, type):
+        self.model = model
+        self.amount = int(amount)
+        self.type = type
 
-class Copiers(Stock):
-    def copier(self):
-        return f'Сделать ксерокопию {self.list_number} раз'
+class Printers(OfficeTech):
+    def __init__(self, model, amount, type='Printers'):
+        super().__init__(model, amount, type=type)
 
-elem1 = Printers('Epson', 5600, 5, 10)
-elem2 = Scanners('Canon', 3500, 5, 10)
-elem3 = Copiers('Xerox', 4000, 1, 15)
-print(elem1.input_stock())
-print(elem2.input_stock())
-print(elem3.input_stock())
-print(elem1.print())
-print(elem2.scan())
-print(elem3.copier())
+class Scanners(OfficeTech):
+    def __init__(self, model, amount, type='Scanners'):
+        super().__init__(model, amount, type=type)
+        self.amount = amount
+
+class Xeroxes(OfficeTech):
+    def __init__(self, model, amount, type='Xeroxes'):
+        super().__init__(model, amount, type=type)
+        self.amount = amount
+
+pri1 = Printers('HP LaserJet Pro M304a', 7)
+pri2 = Printers('Epson L132', 3)
+sca1 = Scanners('CanoScan LiDE 300', 9)
+sca2 = Scanners('HP ScanJet Pro 2500 f1', 4)
+xer1 = Xeroxes('Xerox WorkCentre 3025BI', 6)
+xer2 = Xeroxes('PANTUM P2200', 5)
+
+# Вносим товары в список для отправки на склад.
+stock_list = [pri1, pri2, sca1, sca2, xer1, xer2]
+for el in stock_list:
+    Storage.tech_entry(el)
+print()
+
+# Находим максимальное количество определенной модели техники, чтобы отправить всё со склада.
+checker = []
+for value in Storage.stock.values():
+    checker.append(value[1])
+iterations = max(checker)
+
+# Распределяем технику по отделам пока не опустошим склад полностью.
+for i in range(iterations):
+    Storage.tech_transfer()
+
+print('Вся техника отправлена. Склад пуст.')
+for el, vel in Storage.stock.items():
+    print(el, vel)
